36e533beca48f83ed20fbda671262660bfda3783
digikid <dev@digikid.ru>
2022-06-21 05:24:00 +0400

    Update to 1.3.2

--- src/core/methods/manipulation/val.ts
+++ src/core/methods/manipulation/val.ts
@@ -10,69 +10,51 @@ export type DomValMethod = (
 export default (function (this: IDomElement, value?) {
   if (some<DomValMethodValue>(value, 'null', 'string', 'number', 'array')) {
     return this.each((el) => {
-      if (
-        !some<HTMLInputElement | HTMLSelectElement>(
-          el,
-          'inputElement',
-          'selectElement',
-        )
-      ) {
-        return;
-      }
-
-      if (validate<string[]>(value, 'array')) {
-        if (some<HTMLInputElement>(el, 'radioElement', 'checkboxElement')) {
-          el.checked = value.includes(el.value);
-        }
-
-        if (validate<HTMLSelectElement>(el, 'selectElement')) {
-          const options = Array.from(el.options);
-          const values = options.map((option) => option.value);
-
-          if (el.multiple) {
-            options.forEach((option) => {
-              option.selected = value.includes(option.value);
-            });
-          } else {
-            el.value = [...value].reverse().find((value) => values.includes(value))
-              || '';
+      if ('value' in el) {
+        if (validate<string[]>(value, 'array')) {
+          if (some<HTMLInputElement>(el, 'radioElement', 'checkboxElement')) {
+            el.checked = value.includes(el.value);
           }
-        }
-      } else if (validate<null>(value, 'null')) {
-        if (validate<HTMLSelectElement>(el, 'selectElement')) {
-          const selectedIndex = Array.from(el.options).findIndex(
-            (option) => option.getAttribute('selected') !== null,
-          );
 
-          el.selectedIndex = selectedIndex;
+          if (validate<HTMLSelectElement>(el, 'selectElement')) {
+            const options = Array.from(el.options);
+            const values = options.map((option) => option.value);
+
+            if (el.multiple) {
+              options.forEach((option) => {
+                option.selected = value.includes(option.value);
+              });
+            } else {
+              el.value = [...value].reverse().find((value) => values.includes(value))
+                || '';
+            }
+          }
+        } else if (validate<null>(value, 'null')) {
+          if (validate<HTMLSelectElement>(el, 'selectElement')) {
+            const selectedIndex = Array.from(el.options).findIndex(
+              (option) => option.getAttribute('selected') !== null,
+            );
+
+            el.selectedIndex = selectedIndex;
+          } else {
+            (el as HTMLInputElement).value = '';
+          }
+        } else if (validate<number>(value, 'number')) {
+          (el as HTMLInputElement).value = value.toString();
         } else {
-          el.value = '';
+          (el as HTMLInputElement).value = value;
         }
-      } else if (validate<number>(value, 'number')) {
-        el.value = value.toString();
-      } else {
-        el.value = value;
       }
     });
   }
 
   return reduce.call(this, (el) => {
-    if (
-      some<HTMLInputElement | HTMLSelectElement>(
-        el,
-        'inputElement',
-        'selectElement',
-      )
-    ) {
-      if (validate<HTMLSelectElement>(el, 'selectElement') && el.multiple) {
-        return Array.from(el.options)
-          .filter(({ selected }) => selected)
-          .map(({ value }) => value);
-      }
-
-      return el.value;
+    if (validate<HTMLSelectElement>(el, 'selectElement') && el.multiple) {
+      return Array.from(el.options)
+        .filter(({ selected }) => selected)
+        .map(({ value }) => value);
     }
 
-    return null;
+    return 'value' in el ? (el as HTMLInputElement).value : null;
   });
 } as DomValMethod);
